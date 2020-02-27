4d0cb7dec8dcef764870ebe3d797d975683a4254
matsushii <mtsmath24@yahoo.co.jp>
2020-02-27 15:20:22 +0900

    ToDoを格納する配列およびTableViewの定義を行なった．

--- MyTodoList/ViewController.swift
+++ MyTodoList/ViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+    //  ToDoを格納した配列
+    var todolist = [String]()
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
